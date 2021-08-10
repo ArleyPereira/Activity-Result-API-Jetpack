068a281542b506d9a3160a653f0e0dc9d6fb309d
Arley P. Santana <arley.santana@comprocard.com.br>
2021-08-10 10:58:47 -0300

    Testes com permissões de localização

--- app/src/main/java/com/example/activityresultapi/MainActivity.kt
+++ app/src/main/java/com/example/activityresultapi/MainActivity.kt
@@ -39,6 +39,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    private val requestMultipleAcessLocationPermissionsLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (!permissions.values.contains(false)) {
+                // Após a permissão garantida, podemos chamar o método para iniciar os serviços
+                // de localização.
+                startAcessLocation()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -49,6 +60,10 @@ class MainActivity : AppCompatActivity() {
 
         btnContactsAndCalendar.setOnClickListener {
             checkMultiplePermissions()
+        }
+
+        btnLocalization.setOnClickListener {
+            checkMultipleAccessLocationPermissions()
         }
 
     }
@@ -105,6 +120,39 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Solicita permissão aos serviços de localização
+    private fun checkMultipleAccessLocationPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            val hasAccessCoareLocationPermission = checkPermissionGranted(ACCESS_COARSE_LOCATION)
+            val hasAccessFineLocationPermission = checkPermissionGranted(ACCESS_FINE_LOCATION)
+
+            when {
+                /* Verifica se ambas as permissões foram concedidas, nesse caso podemos
+                 * chamar o método para exibir os dados
+                 */
+                hasAccessCoareLocationPermission and hasAccessFineLocationPermission -> startAcessLocation()
+
+                /* Esse método retorna true quando o usuário negou a pemissão.
+                 * A recomendação, nesse caso, é exibir uma UI explicando
+                 * o motivo da solicitação
+                 */
+                shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION) ||
+                        shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)
+                -> showMessagePermissionAccessLocationDenied()
+
+                /* Chamamos o método launch da instância de ActivityResultLauncher
+                 * que criamos anteriormente para exibir o Dialog de permissões do sistema
+                 */
+
+                else -> requestMultipleAcessLocationPermissionsLauncher.launch(
+                    arrayOf(ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION)
+                )
+
+            }
+        }
+    }
+
     private fun checkPermissionGranted(permission: String) =
         ContextCompat.checkSelfPermission(this, permission) == PERMISSION_GRANTED
 
@@ -116,6 +164,11 @@ class MainActivity : AppCompatActivity() {
     // Implementar lógica de acesso a Contatos e Calendário
     private fun startContactsAndCalendar() {
         Toast.makeText(this, "Contatos e Permissão.", Toast.LENGTH_SHORT).show()
+    }
+
+    // Implementa lógia de acesso aos serviços de localização
+    private fun startAcessLocation() {
+        Toast.makeText(this, "Localização permitida.", Toast.LENGTH_SHORT).show()
     }
 
     private fun showMessagePermissionCameraDenied() {
@@ -160,10 +213,34 @@ class MainActivity : AppCompatActivity() {
         dialog.show()
     }
 
+    private fun showMessagePermissionAccessLocationDenied() {
+        val buider = AlertDialog.Builder(this)
+            .setTitle("Atenção")
+            .setMessage("Para acessar a localização é preciso aceitar a permissão, deseja fazer isso agora?")
+            .setNegativeButton("Não") { _, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton("Sim") { _, _ ->
+                val intent = Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", packageName, null)
+                )
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                dialog.dismiss()
+            }
+
+        dialog = buider.create()
+        dialog.show()
+    }
+
     companion object {
         private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
         private const val READ_CONTACTS_PERMISSION = Manifest.permission.READ_CONTACTS
         private const val READ_CALENDAR_PERMISSION = Manifest.permission.READ_CALENDAR
+
+        private const val ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
+        private const val ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
     }
 
 }
